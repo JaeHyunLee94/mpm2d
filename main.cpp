a8fe9c06262a16601b2f2adeb518ce3831510f63
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2021-10-08 00:12:08 +0900

    TODO: bspline,only gravity

--- main.cpp
+++ main.cpp
@@ -17,25 +17,33 @@ typedef glm::vec2 Vec2;
 typedef glm::vec3 Vec3;
 typedef glm::mat2 Mat2;
 typedef double Scalar;
-struct Particle{
-    Vec2 m_pos;
-    Vec2 m_vel;
-    Scalar mass;
-    Mat2 m_F;
-    Scalar J;
+
+struct GridNode{
+    Vec2 m_vel_i;
+    Vec2 m_force_i;
+    Scalar m_mass_i;
+};
+struct Particle {
+    Vec2 m_pos_p;
+    Vec2 m_vel_p;
+    Vec3 m_force_P;
+    Scalar m_mass_p;
+    Mat2 m_F_p;
+
+    Scalar m_J_p;
 
 };
 //openGL variable
-GLFWwindow* window;
+GLFWwindow *window;
 GLuint VAO;
 GLuint VBO;
 GLuint VBO_line;
 GLuint shader_program_id;
 const float line[]{
-    0.0,0.0,
-    1.0,0.0,
-    1.0,1.0,
-    0.0,1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
 
 };
 
@@ -43,7 +51,8 @@ const float line[]{
 //simulation parameter
 int grid_size;
 Scalar dt;
-Scalar gravity;
+Vec2 gravity;
+Scalar initVolume;
 Scalar radius;
 Scalar particle_mass;
 Scalar dx;
@@ -51,47 +60,62 @@ Scalar inv_dx;
 Vec2 center;
 unsigned int particle_num;
 std::vector<Particle> particles;
-std::vector<std::vector<Vec3>> grid;
-
-
+std::vector<std::vector<GridNode>> grid;
 
 
 //debug function
-GLenum debug_glCheckError(int line)
-{
+GLenum debug_glCheckError(int line) {
     GLenum errorCode;
-    while ((errorCode = glGetError()) != GL_NO_ERROR)
-    {
+    while ((errorCode = glGetError()) != GL_NO_ERROR) {
         std::string error;
-        switch (errorCode)
-        {
-            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
-            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
-            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
-            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
-            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        switch (errorCode) {
+            case GL_INVALID_ENUM:
+                error = "INVALID_ENUM";
+                break;
+            case GL_INVALID_VALUE:
+                error = "INVALID_VALUE";
+                break;
+            case GL_INVALID_OPERATION:
+                error = "INVALID_OPERATION";
+                break;
+            case GL_STACK_OVERFLOW:
+                error = "STACK_OVERFLOW";
+                break;
+            case GL_STACK_UNDERFLOW:
+                error = "STACK_UNDERFLOW";
+                break;
+            case GL_OUT_OF_MEMORY:
+                error = "OUT_OF_MEMORY";
+                break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:
+                error = "INVALID_FRAMEBUFFER_OPERATION";
+                break;
         }
         std::cout << error << " | " << " (" << line << ")" << std::endl;
     }
     return errorCode;
 }
 
-void logGrid(){
+void logGrid() {
 
 };
 
-void logParticle(){
+void logParticle() {
 
+    int i=0;
+    for(auto& p : particles){
+
+        printf("%d th particle m_pos: (%f,%f), m_vel (%f,%f)\n",i,p.m_pos_p.x,p.m_pos_p.y,
+               p.m_vel_p.x,p.m_vel_p.y);
+        i++;
+    }
 };
 
 //opengl function
-int glWindowInit(){
+int glWindowInit() {
     glewExperimental = true; // Needed for core profile
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
         return -1;
     }
 
@@ -102,14 +126,15 @@ int glWindowInit(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
 
 // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 1024, "mpm2d 01", NULL, NULL);
-    if( window == NULL ){
-        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+    window = glfwCreateWindow(1024, 1024, "mpm2d 01", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr,
+                "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window); // Initialize GLEW
-    glewExperimental=true; // Needed in core profile
+    glewExperimental = true; // Needed in core profile
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
         return -1;
@@ -119,29 +144,31 @@ int glWindowInit(){
     return 0;
 
 }
-void glObjectInit(){
+
+void glObjectInit() {
     glPointSize(1.5f);
     debug_glCheckError(102);
-    glGenVertexArrays(1,&VAO);
+    glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    glGenBuffers(1,&VBO);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(Particle)*particles.size(),particles.data(),GL_DYNAMIC_DRAW);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * particles.size(), particles.data(), GL_DYNAMIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(Particle),(void*)offsetof(Particle, m_pos));
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Particle), (void *) offsetof(Particle, m_pos_p));
 
-    glGenBuffers(1,&VBO_line);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO_line);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(float)*8,line,GL_STATIC_DRAW);
+    glGenBuffers(1, &VBO_line);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_line);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8, line, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,(void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void *) 0);
 
 };
-void shaderInit(){
+
+void shaderInit() {
     GLuint m_vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
     GLuint m_fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
-    const char* m_vertex_shader_path = "../shader/VertexShader.glsl";
-    const char* m_fragment_shader_path = "../shader/FragmentShader.glsl";
+    const char *m_vertex_shader_path = "../shader/VertexShader.glsl";
+    const char *m_fragment_shader_path = "../shader/FragmentShader.glsl";
     std::string m_vertex_shader_code;
     std::string m_fragment_shader_code;
 
@@ -156,7 +183,7 @@ void shaderInit(){
         printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n",
                m_vertex_shader_path);
         getchar();
-        return ;
+        return;
     }
     // Read the Fragment Shader code from the file
 
@@ -189,7 +216,7 @@ void shaderInit(){
         std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
         glGetShaderInfoLog(m_vertex_shader_id, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
         printf("%s\n", &VertexShaderErrorMessage[0]);
-        return ;
+        return;
     }
 
     // Compile Fragment Shader
@@ -205,7 +232,7 @@ void shaderInit(){
         std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
         glGetShaderInfoLog(m_fragment_shader_id, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
         printf("%s\n", &FragmentShaderErrorMessage[0]);
-        return ;
+        return;
     }
 
     std::cout << "Shader Successfully compiled\n";
@@ -239,71 +266,77 @@ void shaderInit(){
     std::cout << "Shader Program successfully made\n";
     glUseProgram(shader_program_id);
 
-    glm::vec3 eye(0.5,0.5,1);
-    glm::vec3 lookat(0.5,0.5,0);
-    glm::vec3 up(0,1,0);
-    glm::mat4 m_view_matrix =glm::lookAt(eye,lookat,up);
-    glm::mat4 m_proj_matrix =glm::perspective(glm::radians(60.f),1.0f,0.01f,100.0f);
-    GLint loc_view= glGetUniformLocation(shader_program_id,"viewMat");
-    glUniformMatrix4fv(loc_view,1,GL_FALSE,&m_view_matrix[0][0]);
+    glm::vec3 eye(0.5, 0.5, 1);
+    glm::vec3 lookat(0.5, 0.5, 0);
+    glm::vec3 up(0, 1, 0);
+    glm::mat4 m_view_matrix = glm::lookAt(eye, lookat, up);
+    glm::mat4 m_proj_matrix = glm::perspective(glm::radians(60.f), 1.0f, 0.01f, 100.0f);
+    GLint loc_view = glGetUniformLocation(shader_program_id, "viewMat");
+    glUniformMatrix4fv(loc_view, 1, GL_FALSE, &m_view_matrix[0][0]);
 
-    GLint loc_proj= glGetUniformLocation(shader_program_id,"projMat");
-    glUniformMatrix4fv(loc_proj,1,GL_FALSE,&m_proj_matrix[0][0]);
+    GLint loc_proj = glGetUniformLocation(shader_program_id, "projMat");
+    glUniformMatrix4fv(loc_proj, 1, GL_FALSE, &m_proj_matrix[0][0]);
 
 };
+
 // simulation function
-void render(){
+void render() {
 
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(Particle)*particles.size(),particles.data(),GL_DYNAMIC_DRAW);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(Particle),(void*)offsetof(Particle, m_pos));
-    glDrawArrays(GL_POINTS,0,particle_num);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * particles.size(), particles.data(), GL_DYNAMIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Particle), (void *) offsetof(Particle, m_pos_p));
+    glDrawArrays(GL_POINTS, 0, particle_num);
 
-    glBindBuffer(GL_ARRAY_BUFFER,VBO_line);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,(void*)0);
-    glDrawArrays(GL_LINE_LOOP,0,4);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_line);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void *) 0);
+    glDrawArrays(GL_LINE_LOOP, 0, 4);
 
 
 };
-void simulationInit(){
 
-    dt=1./60;
-    grid_size=100;
-    particle_num=8000;
-    radius=0.05;
+void simulationInit() {
+
+    dt = 1. / 24000;
+    grid_size = 2000;
+    particle_num = 4000;
+    radius = 0.05;
+    gravity=Vec2{0,-9.8};
+    initVolume=0.2f; //TODO
     particles.resize(particle_num);
-    particle_mass=1.0;
-    dx=1./grid_size;
-    inv_dx=1./dx;
-    center.x=0.5;
-    center.y=0.7;
+    particle_mass = 1.0;
+    dx = 1. / grid_size;
+    inv_dx = 1. / dx;
+    center.x = 0.5;
+    center.y = 0.7;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(0, 9999);
 
     //particle init
-    for(int i=0;i< particle_num;i++){
-        int random_num=dis(gen);
-        Scalar r = (radius/10000.0)*(Scalar)dis(gen);
-        Scalar theta=(360.0/10000.0)*(Scalar)dis(gen);
-        particles[i].m_pos.x = center.x + r*cos(theta);
-        particles[i].m_pos.y = center.y + r*sin(theta);
-        particles[i].m_vel.x=0;
-        particles[i].m_vel.y=0;
-        particles[i].m_F=Mat2(1.0);
-        particles[i].J=1;
-        particles[i].mass=particle_mass;
+    for (int i = 0; i < particle_num; i++) {
+        int random_num = dis(gen);
+        Scalar r = (radius / 10000.0) * (Scalar) dis(gen);
+        Scalar theta = (360.0 / 10000.0) * (Scalar) dis(gen);
+        particles[i].m_pos_p.x = center.x + r * cos(theta);
+        particles[i].m_pos_p.y = center.y + r * sin(theta);
+        particles[i].m_vel_p.x = 0;
+        particles[i].m_vel_p.y = 0;
+        particles[i].m_F_p = Mat2(1.0);
+        particles[i].m_J_p = 1;
+        particles[i].m_mass_p = particle_mass;
 
 
     }
 
     //grid init
     grid.resize(grid_size);
-    for(auto& g: grid){
+    for (auto &g: grid) {
         g.resize(grid_size);
-        for(int i=0;i<grid_size;i++){
+        for (int i = 0; i < grid_size; i++) {
             //vel + mass
-            g[i]=Vec3(0,0,particle_mass);
+            g[i].m_vel_i = Vec2 (0);
+            g[i].m_force_i=Vec2(0);
+            g[i].m_mass_i=0;
         }
     }
 
@@ -312,108 +345,165 @@ void simulationInit(){
 
 
 //cubic B-spline
-Scalar N(Scalar x){
-    x=abs(x);
-    if(x>=0 && x<1){
-        return 0.5* pow(x,3)- pow(x,2)+2./3;
-    }else if(x>=1 && x<2){
-        return -(1./6)* pow(x,3)+ pow(x,2) - 2*x+4./3;
-    }else{
+Scalar N(Scalar x) {
+    x = abs(x);
+    if (x >= 0 && x < 1) {
+        return 0.5 * pow(x, 3) - pow(x, 2) + 2. / 3;
+    } else if (x >= 1 && x < 2) {
+        return -(1. / 6) * pow(x, 3) + pow(x, 2) - 2 * x + 4. / 3;
+    } else {
         return 0;
     }
 }
 
-Scalar diff_N(Scalar x){
+Scalar diff_N(Scalar x) {
 
 
-    Scalar ret=0;
-    if(x>=0 && x<1){
-        ret= 1.5* pow(abs(x),2)- 2*pow(abs(x),2);
-    }else if(x>=1 && x<2){
-        ret -0.5* pow(abs(x),2)+ 2*pow(abs(x),2) -2;
+    Scalar ret = 0;
+    if (x >= 0 && x < 1) {
+        ret = 1.5 * pow(abs(x), 2) - 2 * pow(abs(x), 2);
+    } else if (x >= 1 && x < 2) {
+        ret - 0.5 * pow(abs(x), 2) + 2 * pow(abs(x), 2) - 2;
     }
 
-    return x>0 ? ret : -ret;
+    return x > 0 ? ret : -ret;
 
 
 }
-
 
 //weight
-Scalar W(Vec2 x_p,int grid_i,int grid_j){
-    Scalar fx= inv_dx*(x_p.x-grid_i*dx);
-    Scalar fy= inv_dx*(x_p.y-grid_j*dx);
-    return N(fx)*N(fy);
+Scalar W(Vec2 x_p, int grid_i, int grid_j) {
+    Scalar fx = inv_dx * (x_p.x - grid_i * dx);
+    Scalar fy = inv_dx * (x_p.y - grid_j * dx);
+    return N(fx) * N(fy);
 
 }
 
-Vec2 grad_W(Vec2 x_p,int grid_i,int grid_j){
+Vec2 grad_W(Vec2 x_p, int grid_i, int grid_j) {
 
-    Scalar fx= inv_dx*(x_p.x-grid_i*dx);
-    Scalar fy= inv_dx*(x_p.y-grid_j*dx);
+    Scalar fx = inv_dx * (x_p.x - grid_i * dx);
+    Scalar fy = inv_dx * (x_p.y - grid_j * dx);
 
     Vec2 ret(0);
-    ret.x=N(fx)*inv_dx* diff_N(fx);
-    ret.y=N(fy)*inv_dx* diff_N(fy);
+    ret.x = N(fx) * inv_dx * diff_N(fx);
+    ret.y = N(fy) * inv_dx * diff_N(fy);
 
     return ret;
 
 }
 
 
-void initGrid(){
-    for(int i=0;i<grid_size;i++){
-        for(int j=0;j<grid_size;j++){
-            grid[i][j].x=0;
-            grid[i][j].y=0;
-            //mass
-            grid[i][j].z=0;
-        }
-    }
-
-};
-void p2g(){
-
-    for(auto& p : particles){
-
-
-
-
-
-
-    }
-
-};
-void updateGridVel(){
-
-
-
-};
-void gridCollision(){
-
-};
-void g2p(){
-
-    for(int i=0;i<grid_size;i++){
-
-        for(int j=0;j<grid_size;j++){
-
-
+void initGrid() {
+    for (int i = 0; i < grid_size; i++) {
+        for (int j = 0; j < grid_size; j++) {
+            grid[i][j].m_vel_i=Vec2(0);
+            grid[i][j].m_mass_i=0;
+            grid[i][j].m_force_i=Vec2(0);
 
         }
     }
 
 };
-void updateParticle(){
+
+void p2g() {
+
+    for (auto &p : particles) {
+
+        int base_x = static_cast<int> (std::floor(p.m_pos_p.x *inv_dx));
+        int base_y = static_cast<int> (std::floor(p.m_pos_p.y*inv_dx));
+
+
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+
+                int coord_x = base_x + i;
+                int coord_y = base_y + j;
+                //check boundary
+                if (coord_x < 0 || coord_y < 0 || coord_x >= grid_size || coord_y >= grid_size) continue;
+                Scalar Weight =  W(p.m_pos_p, coord_x, coord_y);
+
+
+                grid[coord_x][coord_y].m_vel_i = p.m_vel_p * (float)(p.m_mass_p* Weight);
+                grid[coord_x][coord_y].m_mass_i = p.m_mass_p *Weight;
+
+                grid[coord_x][coord_y].m_vel_i/= grid[coord_x][coord_y].m_mass_i;
+
+
+            }
+        }
+
+
+    }
+
+};
+
+void updateGridVel() {
+
+
+    for(int i=0;i<grid_size;i++){
+        for(int j=0;j<grid_size;j++){
+
+
+            grid[i][j].m_vel_i += (gravity) *(float)dt;
+
+        }
+
+    }
+
 
 
 };
 
-void particleCollision(){
+void gridCollision() {
 
 };
 
-void step(){
+void g2p() {
+
+    for (auto &p : particles) {
+
+        int base_x = static_cast<int> (std::floor(p.m_pos_p.x *inv_dx));
+        int base_y = static_cast<int> (std::floor(p.m_pos_p.y*inv_dx));
+
+
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+
+                int coord_x = base_x + i;
+                int coord_y = base_y + j;
+                //check boundary
+                if (coord_x < 0 || coord_y < 0 || coord_x >= grid_size || coord_y >= grid_size) continue;
+                Scalar Weight =  W(p.m_pos_p, coord_x, coord_y);
+
+
+                p.m_vel_p += grid[coord_x][coord_y].m_vel_i*(float)Weight;
+
+
+
+            }
+        }
+
+
+    }
+
+};
+
+void updateParticle() {
+    for (auto &p : particles) {
+
+        //explicit
+        p.m_pos_p+=p.m_vel_p*(float)dt;
+
+    }
+
+
+};
+
+void particleCollision() {
+
+};
+
+void step() {
 
     initGrid();
     p2g();
@@ -426,31 +516,32 @@ void step(){
 }
 
 
+int main() {
 
-
-int main(){
-
-
+//TODO: cubic b spline 다른걸로
+//TODO: p2g 말고 그냥 gravity update
     glWindowInit();
     shaderInit();
     simulationInit();
     glObjectInit();
 
-    do{
+    int frame=0;
+    do {
 
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
 
         step();
+        //logParticle();
         render();
-
+        std::cout << "frame: "<< frame<< "\n";
+        frame++;
         glfwSwapBuffers(window);
 
 
-
     } // Check if the ESC key was pressed or the window was closed
-    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-           glfwWindowShouldClose(window) == 0 );
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0);
 
 
     return 0;
